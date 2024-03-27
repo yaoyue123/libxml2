af82d314bb5aefa2854d9281402a189f7eeb573b
yaoyue123 <2742949404@qq.com>
2024-03-27 15:58:17 +0800

    删除build

--- build/CMakeFiles/LibXml2.dir/nanoftp.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/LibXml2.dir/nanoftp.c.o: \
-  /root/workspace/mllib-sandbox/test/libxml2/nanoftp.c \
-  /root/workspace/mllib-sandbox/test/libxml2/libxml.h \
-  /root/workspace/mllib-sandbox/test/libxml2/build/config.h \
-  /root/workspace/mllib-sandbox/test/libxml2/build/libxml/xmlversion.h \
-  /root/workspace/mllib-sandbox/test/libxml2/include/libxml/xmlexports.h
